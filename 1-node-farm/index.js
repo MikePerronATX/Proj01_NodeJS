e1f406a42a1ab9fbe2fbab9f48592809ba015ff9
Michael Perron <mikeperron.atx@gmail.com>
2022-02-17 14:11:01 -0600

    asynchronous code

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 
+//blocking synchronous
 const textIn  = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./starter/txt/output.txt', textOut);
 console.log('File written!');
+
+//non-blocking asynchronous
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) =>{
+    console.log(data);
+});
