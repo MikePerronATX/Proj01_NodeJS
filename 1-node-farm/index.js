f74eba12d367fe3ff501aa92a54a0d029f39da4d
Michael Perron <mikeperron.atx@gmail.com>
2022-02-17 14:52:52 -0600

    practicing callbacks

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 
 //blocking synchronous
-const textIn  = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+/* const textIn  = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./starter/txt/output.txt', textOut);
-console.log('File written!');
+console.log('File written!'); */
 
 //non-blocking asynchronous
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) =>{
-    console.log(data);
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) =>{
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) =>{
+            console.log(data3);
+
+            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written 😄');
+            })
+        });
+    });
 });
+console.log("Will read file");
