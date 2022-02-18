01d11e18d40b94ad0b3537ce1a74774d6368f440
Michael Perron <mikeperron.atx@gmail.com>
2022-02-17 22:10:59 -0600

    working with routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ///////////////
 //Files
@@ -34,9 +35,19 @@ console.log("Will read file"); */
 //Server
 ///////////////
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!');
-});
+    //console.log(req.url);
+    const pathname = req.url;
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.end('This is overview');
+    } else if (pathname === '/product'){
+        res.end('This is the product');
+        }
+    });
+
+
+/*     res.end('Hello from the server!');
+}); */
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000' );
